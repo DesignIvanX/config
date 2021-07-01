651c8dddf647b19147656ed541c15e65115eda8b
XDesignIvanx <ivandcdesign@gmail.com>
2021-07-01 05:32:05 +0200

    v1.0

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/style/App.scss'
+
+function HolaMundo() {
+    return(
+        <h1>Hola Mundo</h1>
+    )
+}
+
+export default HolaMundo;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HolaMundo from './components/HolaMundo';
+
+const app = document.querySelector('#app')
+
+ReactDOM.render(<HolaMundo />, app)
